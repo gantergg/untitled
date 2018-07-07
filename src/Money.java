d917d5e11141fdd1103d1666f78de2ccb3b5bd1b
ganter <ankernz@gmail.com>
2018-07-07 15:07:25 +0300

    L

---
+++ src/Money.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Money implements Realize{
+    Scanner scanner = new Scanner(System.in);
+    public long rub = scanner.nextLong();
+    public String kop = scanner.next();
+
+
+
+   public void summa(){
+
+    }
+
+    public void substract(){
+
+    }
+
+    public void divisionNumber(){
+
+    }
+
+    public void divisionMoney(){
+
+    }
+
+    public void multiplyNumber(){
+
+    }
+
+    public void multyplyMoney(){
+
+    }
+}
---
+++ src/Realize.java
@@ -0,0 +1,8 @@
+public interface Realize {
+        void summa();
+        void substract();
+        void divisionNumber();
+        void divisionMoney();
+        void multiplyNumber();
+        void multyplyMoney();
+    }
